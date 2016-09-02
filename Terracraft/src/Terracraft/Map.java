2b0f9a0db3b3f0234c1adcaf1318b486c4839f24
Plumsklo <daumenknick123>
2016-09-02 21:54:05 +0200

    3/24523 schneemann

--- Terracraft/src/Terracraft/Map.java
+++ Terracraft/src/Terracraft/Map.java
@@ -41,10 +41,6 @@ public class Map {
 			g.drawString("Andere Positionen:", Game.player.getX() + 450, Game.player.getY() + 114 - 400 - 74);
 		}
 		g.drawImage(this.getBufferedImage(map),  Game.player.getX() + 200,Game.player.getY() - 430,360,220,null);
-<<<<<<< HEAD
-=======
-		
->>>>>>> origin/master
 		online = false;
 		playerCounter = 0;
 		
